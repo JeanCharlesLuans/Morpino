dc4d50dfc3becd7053cef05ae2bf79adad98d999
jeancharlesluans <jean-charles.luans@iut-rodez.fr>
2020-01-22 20:46:24 +0100

    Premier Commit initialisation du projet

---
+++ cmake-build-debug/CMakeFiles/Morpino.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Morpino.dir/main.cpp.obj"
+  "Morpino.exe"
+  "Morpino.exe.manifest"
+  "Morpino.pdb"
+  "libMorpino.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Morpino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
